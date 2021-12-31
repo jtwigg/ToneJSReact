fd1e0efe2976e146e9e237784673131691f3211e
John Twigg <john.twigg@gmail.com>
2021-12-31 12:04:28 -0600

    Adding Tone JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import * as Tone from 'tone'
+
 
 function App() {
+
+  const onAction = () => {
+    Tone.start()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => onAction()} >Start</button>
       </header>
     </div>
   );
